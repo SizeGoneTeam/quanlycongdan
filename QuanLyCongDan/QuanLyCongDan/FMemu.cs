3f40f1685599e40a8c0e87e3dc5150905f4140f2
lulueve3 <89736351+lulueve3@users.noreply.github.com>
2023-03-15 23:39:28 +0700

    Revert "Merge branch 'khaisinh' of https://github.com/SizeGoneTeam/quanlycongdan into khaisinh"
    This reverts commit 10a59fef4d3c7ae59516a0e59c708eb944139b8e, reversing
    changes made to 59a437c538b70dc23fb457e481ac7e5ebab5f879.

--- QuanLyCongDan/QuanLyCongDan/FMemu.cs
+++ QuanLyCongDan/QuanLyCongDan/FMemu.cs
@@ -24,15 +24,16 @@ namespace QuanLyCongDan
             fCongDan.Show();
         }
 
-        private void btnKhaiSinh_Click(object sender, EventArgs e)
-        {
-            FKhaiSinh f = new FKhaiSinh();
-            f.Show();
-	}
         private void btnCCCD_Click(object sender, EventArgs e)
         {
             FCCCD fCCCD = new FCCCD();
             fCCCD.Show();
+        }
+
+        private void btnKhaiSinh_Click(object sender, EventArgs e)
+        {
+            FKhaiSinh f = new FKhaiSinh();
+            f.Show();
         }
     }
 }
