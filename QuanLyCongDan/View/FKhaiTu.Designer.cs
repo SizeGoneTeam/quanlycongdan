1483e6a58bd29c2a1386f41ed089e6b5f9f88165
Trung Hieu <trunghieusn2002@gmail.com>
2023-03-23 01:33:24 +0700

    Thêm form khai tử

---
+++ QuanLyCongDan/View/FKhaiTu.Designer.cs
@@ -0,0 +1,409 @@
+﻿namespace QuanLyCongDan.View
+{
+    partial class FKhaiTu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnTimKiemCongDan = new System.Windows.Forms.Button();
+            this.btnTimKiemNguoiYeuCau = new System.Windows.Forms.Button();
+            this.txtIDCongDan = new System.Windows.Forms.TextBox();
+            this.txtIDNguoiYeuCau = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtHoVaTenNguoiYeuCau = new System.Windows.Forms.TextBox();
+            this.txtHoVaTenCongDan = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtQuanHe = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.pkThoiGianChet = new System.Windows.Forms.DateTimePicker();
+            this.pkNgayThucHien = new System.Windows.Forms.DateTimePicker();
+            this.txtNoiChet = new System.Windows.Forms.TextBox();
+            this.txtNoiDangKy = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtNguyenNhan = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnTimKiem);
+            this.panel1.Controls.Add(this.txtTimKiem);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Location = new System.Drawing.Point(333, 30);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(75, 23);
+            this.btnTimKiem.TabIndex = 2;
+            this.btnTimKiem.Text = "Tìm kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Location = new System.Drawing.Point(200, 34);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(100, 20);
+            this.txtTimKiem.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Khai tử số:";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtNoiDangKy);
+            this.panel2.Controls.Add(this.txtNguyenNhan);
+            this.panel2.Controls.Add(this.txtNoiChet);
+            this.panel2.Controls.Add(this.pkNgayThucHien);
+            this.panel2.Controls.Add(this.pkThoiGianChet);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.label11);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.btnTimKiemCongDan);
+            this.panel2.Controls.Add(this.btnTimKiemNguoiYeuCau);
+            this.panel2.Controls.Add(this.txtHoVaTenCongDan);
+            this.panel2.Controls.Add(this.txtIDCongDan);
+            this.panel2.Controls.Add(this.txtHoVaTenNguoiYeuCau);
+            this.panel2.Controls.Add(this.txtQuanHe);
+            this.panel2.Controls.Add(this.txtIDNguoiYeuCau);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(12, 106);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(776, 227);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnTimKiemCongDan
+            // 
+            this.btnTimKiemCongDan.Location = new System.Drawing.Point(533, 25);
+            this.btnTimKiemCongDan.Name = "btnTimKiemCongDan";
+            this.btnTimKiemCongDan.Size = new System.Drawing.Size(75, 23);
+            this.btnTimKiemCongDan.TabIndex = 4;
+            this.btnTimKiemCongDan.Text = "Tìm kiếm";
+            this.btnTimKiemCongDan.UseVisualStyleBackColor = true;
+            this.btnTimKiemCongDan.Click += new System.EventHandler(this.btnTimKiemCongDan_Click);
+            // 
+            // btnTimKiemNguoiYeuCau
+            // 
+            this.btnTimKiemNguoiYeuCau.Location = new System.Drawing.Point(242, 25);
+            this.btnTimKiemNguoiYeuCau.Name = "btnTimKiemNguoiYeuCau";
+            this.btnTimKiemNguoiYeuCau.Size = new System.Drawing.Size(75, 23);
+            this.btnTimKiemNguoiYeuCau.TabIndex = 4;
+            this.btnTimKiemNguoiYeuCau.Text = "Tìm kiếm";
+            this.btnTimKiemNguoiYeuCau.UseVisualStyleBackColor = true;
+            this.btnTimKiemNguoiYeuCau.Click += new System.EventHandler(this.btnTimKiemNguoiYeuCau_Click);
+            // 
+            // txtIDCongDan
+            // 
+            this.txtIDCongDan.Location = new System.Drawing.Point(427, 25);
+            this.txtIDCongDan.Name = "txtIDCongDan";
+            this.txtIDCongDan.Size = new System.Drawing.Size(100, 20);
+            this.txtIDCongDan.TabIndex = 3;
+            // 
+            // txtIDNguoiYeuCau
+            // 
+            this.txtIDNguoiYeuCau.Location = new System.Drawing.Point(135, 25);
+            this.txtIDNguoiYeuCau.Name = "txtIDNguoiYeuCau";
+            this.txtIDNguoiYeuCau.Size = new System.Drawing.Size(100, 20);
+            this.txtIDNguoiYeuCau.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(349, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Người tử";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(52, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Quan hệ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Người yêu cầu";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnXoa);
+            this.panel3.Controls.Add(this.btnSua);
+            this.panel3.Controls.Add(this.btnThem);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 350);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(800, 100);
+            this.panel3.TabIndex = 2;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(504, 43);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 2;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(321, 42);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 1;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(132, 42);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 0;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(55, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Họ và tên";
+            // 
+            // txtHoVaTenNguoiYeuCau
+            // 
+            this.txtHoVaTenNguoiYeuCau.Location = new System.Drawing.Point(135, 59);
+            this.txtHoVaTenNguoiYeuCau.Name = "txtHoVaTenNguoiYeuCau";
+            this.txtHoVaTenNguoiYeuCau.ReadOnly = true;
+            this.txtHoVaTenNguoiYeuCau.Size = new System.Drawing.Size(100, 20);
+            this.txtHoVaTenNguoiYeuCau.TabIndex = 2;
+            // 
+            // txtHoVaTenCongDan
+            // 
+            this.txtHoVaTenCongDan.Location = new System.Drawing.Point(429, 66);
+            this.txtHoVaTenCongDan.Name = "txtHoVaTenCongDan";
+            this.txtHoVaTenCongDan.ReadOnly = true;
+            this.txtHoVaTenCongDan.Size = new System.Drawing.Size(100, 20);
+            this.txtHoVaTenCongDan.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(349, 66);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Họ và tên";
+            // 
+            // txtQuanHe
+            // 
+            this.txtQuanHe.Location = new System.Drawing.Point(135, 124);
+            this.txtQuanHe.Name = "txtQuanHe";
+            this.txtQuanHe.Size = new System.Drawing.Size(100, 20);
+            this.txtQuanHe.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(281, 130);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Thời gian chết";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(34, 168);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Nơi chết";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(29, 202);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(65, 13);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Nơi đăng ký";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(281, 202);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(79, 13);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "Ngày thực hiện";
+            // 
+            // pkThoiGianChet
+            // 
+            this.pkThoiGianChet.Location = new System.Drawing.Point(409, 130);
+            this.pkThoiGianChet.Name = "pkThoiGianChet";
+            this.pkThoiGianChet.Size = new System.Drawing.Size(200, 20);
+            this.pkThoiGianChet.TabIndex = 7;
+            // 
+            // pkNgayThucHien
+            // 
+            this.pkNgayThucHien.Location = new System.Drawing.Point(409, 196);
+            this.pkNgayThucHien.Name = "pkNgayThucHien";
+            this.pkNgayThucHien.Size = new System.Drawing.Size(200, 20);
+            this.pkNgayThucHien.TabIndex = 8;
+            // 
+            // txtNoiChet
+            // 
+            this.txtNoiChet.Location = new System.Drawing.Point(95, 161);
+            this.txtNoiChet.Name = "txtNoiChet";
+            this.txtNoiChet.Size = new System.Drawing.Size(100, 20);
+            this.txtNoiChet.TabIndex = 9;
+            // 
+            // txtNoiDangKy
+            // 
+            this.txtNoiDangKy.Location = new System.Drawing.Point(113, 195);
+            this.txtNoiDangKy.Name = "txtNoiDangKy";
+            this.txtNoiDangKy.Size = new System.Drawing.Size(100, 20);
+            this.txtNoiDangKy.TabIndex = 10;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(311, 168);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(71, 13);
+            this.label11.TabIndex = 6;
+            this.label11.Text = "Nguyên nhân";
+            // 
+            // txtNguyenNhan
+            // 
+            this.txtNguyenNhan.Location = new System.Drawing.Point(388, 165);
+            this.txtNguyenNhan.Name = "txtNguyenNhan";
+            this.txtNguyenNhan.Size = new System.Drawing.Size(100, 20);
+            this.txtNguyenNhan.TabIndex = 9;
+            // 
+            // FKhaiTu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FKhaiTu";
+            this.Text = "FKhaiTu";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.Button btnTimKiemCongDan;
+        private System.Windows.Forms.Button btnTimKiemNguoiYeuCau;
+        private System.Windows.Forms.TextBox txtIDCongDan;
+        private System.Windows.Forms.TextBox txtIDNguoiYeuCau;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtHoVaTenCongDan;
+        private System.Windows.Forms.TextBox txtHoVaTenNguoiYeuCau;
+        private System.Windows.Forms.TextBox txtQuanHe;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtNoiDangKy;
+        private System.Windows.Forms.TextBox txtNoiChet;
+        private System.Windows.Forms.DateTimePicker pkNgayThucHien;
+        private System.Windows.Forms.DateTimePicker pkThoiGianChet;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtNguyenNhan;
+        private System.Windows.Forms.Label label11;
+    }
+}
---
+++ QuanLyCongDan/View/FKhaiTu.cs
@@ -0,0 +1,204 @@
+﻿using QuanLyCongDan.DAO;
+using QuanLyCongDan.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Week2;
+
+namespace QuanLyCongDan.View
+{
+    public partial class FKhaiTu : Form
+    {
+        private KhaiTu khaiTu;
+        private CongDan congDan;
+        private CongDan nguoiYeuCau;
+
+        private KhaiTuDAO khaiTuDAO = new KhaiTuDAO();
+        private CongDanDAO congDanDAO = new CongDanDAO();
+
+        public FKhaiTu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if (congDan != null && nguoiYeuCau != null)
+            {
+                if (khaiTuDAO.Them(new KhaiTu(
+                            int.Parse(congDan.Id),
+                            int.Parse(nguoiYeuCau.Id),
+                            txtQuanHe.Text,
+                            pkThoiGianChet.Value,
+                            txtNoiChet.Text,
+                            txtNguyenNhan.Text,
+                            txtNoiDangKy.Text,
+                            pkNgayThucHien.Value.Date)
+                        )
+                    )
+                {
+                    MessageBox.Show("Thêm thành công!");
+                }
+                else
+                {
+                    MessageBox.Show("Thêm thất bại!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Thiếu thông tin!");
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            if (khaiTu != null && congDan != null && nguoiYeuCau != null)
+            {
+                if (khaiTuDAO.Sua(new KhaiTu(
+                            khaiTu.ID,
+                            int.Parse(congDan.Id),
+                            int.Parse(nguoiYeuCau.Id),
+                            txtQuanHe.Text,
+                            pkThoiGianChet.Value,
+                            txtNoiChet.Text,
+                            txtNguyenNhan.Text,
+                            txtNoiDangKy.Text,
+                            pkNgayThucHien.Value.Date)
+                        )
+                    )
+                {
+                    MessageBox.Show("Sửa thành công!");
+                }
+                else
+                {
+                    MessageBox.Show("Sửa thất bại!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Thiếu thông tin!");
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (khaiTu != null)
+            {
+                if (khaiTuDAO.Xoa(khaiTu.ID))
+                {
+                    MessageBox.Show("Xóa thành công!");
+                }
+                else
+                {
+                    MessageBox.Show("Xóa thất bại!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Xóa thất bại!");
+            }
+        }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            int iD;
+            
+            if (int.TryParse(txtTimKiem.Text, out iD))
+            {
+                khaiTu = khaiTuDAO.TimKiem(iD);
+                if (khaiTu is null)
+                {
+                    MessageBox.Show("Không tìm thấy!");
+
+                    Controls.Clear();
+                    InitializeComponent();
+                }
+                else
+                {
+                    congDan = congDanDAO.TimKiem(khaiTu.IDCongDan.ToString());
+                    nguoiYeuCau = congDanDAO.TimKiem(khaiTu.IDNguoiYeuCau.ToString());
+                    
+                    updateContent();
+                }
+            }
+            else
+            {
+                MessageBox.Show("ID không hợp lệ!");
+
+                khaiTu = null;
+                congDan = null;
+                nguoiYeuCau = null;
+
+                Controls.Clear();
+                InitializeComponent();
+            }
+        }
+
+        private void btnTimKiemCongDan_Click(object sender, EventArgs e)
+        {
+            int iD;
+            
+            if (int.TryParse(txtIDCongDan.Text, out iD))
+            {
+                congDan = congDanDAO.TimKiem(iD.ToString());
+
+                updateContentCongDan();
+            }
+            else
+            {
+                MessageBox.Show("ID công dân không hợp lệ!");
+
+                congDan = null;
+                txtHoVaTenCongDan.Clear();
+            }
+        }
+
+        private void btnTimKiemNguoiYeuCau_Click(object sender, EventArgs e)
+        {
+            int iD;
+
+            if (int.TryParse(txtIDNguoiYeuCau.Text, out iD))
+            {
+                nguoiYeuCau = congDanDAO.TimKiem(iD.ToString());
+
+                updateContentNguoiYeuCau();
+            }
+            else
+            {
+                MessageBox.Show("ID người yêu cầu không hợp lệ!");
+
+                nguoiYeuCau = null;
+                txtHoVaTenNguoiYeuCau.Clear();
+            }
+        }
+
+        private void updateContent()
+        {
+            txtQuanHe.Text = khaiTu.QuanHe;
+            txtNoiChet.Text = khaiTu.NoiChet;
+            txtNoiDangKy.Text = khaiTu.NoiDangKy;
+            txtNguyenNhan.Text = khaiTu.NguyenNhan;
+            pkThoiGianChet.Value = khaiTu.ThoiGianChet;
+            pkNgayThucHien.Value = khaiTu.NgayThucHien;
+            
+            updateContentCongDan();
+            updateContentNguoiYeuCau();
+        }
+
+        private void updateContentCongDan()
+        {
+            txtHoVaTenCongDan.Text = congDan.HoTen;
+        }
+
+        private void updateContentNguoiYeuCau()
+        {
+            txtHoVaTenNguoiYeuCau.Text = nguoiYeuCau.HoTen;
+        }
+    }
+}
